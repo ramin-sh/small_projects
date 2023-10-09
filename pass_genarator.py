0d11e3ea0ccedbd54793d7d20ed55c94d3bdd55d
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2023-10-09 20:48:34 +0330

    Create pass_genarator.py

---
+++ pass_genarator.py
@@ -0,0 +1,20 @@
+import string
+import random
+def generate_pass():
+    password = []
+    for i in range(5):
+        alpha = random.choice(string.ascii_letters)
+        symbol = random.choice(string.punctuation)
+        numbers = random.choice(string.digits)
+        password.append(alpha)
+        password.append(symbol)
+        password.append(numbers)
+        passwords = " ".join(str(x)for x in password)
+        label.config(text=passwords)
+root = tk.Tk()
+root.geometry("400x300")
+button = tk.Button(root, text="تولید پسورد", command=generate_pass)
+button.grid(row=1, column=1)
+label = tk.Label(root, font=("times", 15, "bold"))
+label.grid(row=4, column=2)
+root.mainloop()
